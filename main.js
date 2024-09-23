0aa6356307598a5d434f3d86cf7fe776e1495e0d
Mritunjay Pandey <pmritunjay9555@gmail.com>
2024-09-23 13:33:39 +0530

    implemented tree

--- main.js
+++ main.js
@@ -28,7 +28,36 @@ switch(command){
 }
 
 function treeFn(dirPath){
-    console.log("Tree command implemented for", dirPath);
+    if (dirPath  == undefined){
+        console.log("Kidly  enter the path");
+        return;
+    }
+    else{
+        let doesExist = fs.existsSync(dirPath);
+        if(doesExist){
+            treeHelper(dirPath,"")
+        }
+        else{
+            console.log("Kindly enter  the correct path");
+            return;
+        }
+    }
+}
+function treeHelper(dirPath, indent){
+    let isFile = fs.lstatSync(dirPath).isFile();
+    if(isFile == true){
+        let fileName = path.basename(dirPath);
+        console.log(indent+"|-----"+fileName)
+    }
+    else{
+        let dirName = path.basename(dirPath);
+        console.log(indent+"|____"+dirName);
+        let childrens = fs.readdirSync(dirPath);
+        for(let i = 0; i < childrens.length; i++){
+            let childPath = path.join(dirPath, childrens[i]);
+            treeHelper(childPath,indent+"\t");
+        }
+    }
 }
 function organizeFn(dirPath){
     // console.log("organize command implemented for", dirPath);
